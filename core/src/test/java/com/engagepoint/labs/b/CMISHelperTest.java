1288db11dab51e800ce2a27ab0c43720f749d222
vladimirovcharov <vladimir.ovcharov@engagepoint.com>
2013-06-18 13:25:30 +0300

    CMISHelperTest created

---
+++ core/src/test/java/com/engagepoint/labs/b/CMISHelperTest.java
@@ -0,0 +1,11 @@
+package com.engagepoint.labs.b;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vladimir.ovcharov
+ * Date: 6/18/13
+ * Time: 1:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CMISHelperTest {
+}

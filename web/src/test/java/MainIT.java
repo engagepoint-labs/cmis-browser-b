8e9e9a618792f435eb3d306e79255a2eb87413c0
artemkhudashchov <artem.khudashchov@engagepoint.com>
2013-06-21 18:56:38 +0300

    add IC test

--- web/src/test/java/MainIT.java
+++ web/src/test/java/MainIT.java
@@ -1,12 +1,9 @@
 
-import com.engagepoint.labs.b.core.BrowserItem;
 import com.engagepoint.labs.b.core.CMISHelper;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainIT {
@@ -14,17 +11,15 @@ public class MainIT {
     private CMISHelper cmisHelper = new CMISHelper();
 
 
-    @Test
-    public void listTest() throws Exception{
-       List<BrowserItem> arrayList = new ArrayList<BrowserItem>();
-        for (int i = 0; i <arrayList.size() ; i++) {
-            arrayList.add(cmisHelper.getRootFolder().get(i));
-        }
-        assertNotNull(arrayList);
+    @Test(expected = NullPointerException.class)
+    public void test_getRootFoolder_is_empty(){
+
+        cmisHelper.getRootFolder().get(0);
+
     }
 
     @Test
-    public void cmisTest() throws Exception{
+    public void test_getVersion() throws Exception{
         cmisHelper.connect();
         String version = "CMIS_1_0";
         assertEquals(version,cmisHelper.getVersion());

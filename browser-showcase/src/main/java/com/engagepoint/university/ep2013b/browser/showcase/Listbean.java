b83ecc8364668bf793d8cc3d4ffdcbf1e902516a
irynadomachuk <iryna.domachuk@engagepoint.com>
2013-06-26 13:13:47 +0300

    new featureLABB-31 - injected beans (user data bean & list bean)

---
+++ browser-showcase/src/main/java/com/engagepoint/university/ep2013b/browser/showcase/Listbean.java
@@ -0,0 +1,117 @@
+package com.engagepoint.university.ep2013b.browser.showcase;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: iryna.domachuk
+ * Date: 6/20/13
+ * Time: 6:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@ManagedBean
+@SessionScoped
+public class Listbean implements Serializable {
+
+  private ArrayList<User> list = new ArrayList<User>();
+  private User selected = new User();
+
+    /*
+    @ManagedProperty(value="#{users}")
+    private UserDB users;
+   */
+
+
+    public Listbean() {
+
+    }
+
+    @PostConstruct
+    public void init(){
+
+        int id = 0;
+
+        User us = new User(++id, "Ivan", "Petrov");
+        list.add(us);
+        us = new User(++id, "Dmitro","Ivanov");
+        list.add(us);
+        us = new User(++id, "Danila","Severny");
+        list.add(us);
+        us = new User(++id, "Pavlo","Demidov");
+        list.add(us);
+        us = new User(++id, "Sergio","Suvorov");
+        list.add(us);
+        us = new User(++id, "Fedir","Olenev");
+        list.add(us);
+        us = new User(++id, "Stas","Kamarin");
+        list.add(us);
+
+    }
+
+
+
+    public ArrayList<User> getList() {
+        return list;
+    }
+
+
+
+    public void setList(ArrayList<User> list) {
+        this.list = list;
+    }
+
+    public User getSelected() {
+        return selected;
+    }
+
+    public void setSelected(User selected) {
+        this.selected = selected;
+    }
+
+
+    public String edit(User us){
+
+        selected = new User();
+        selected.setId(us.getId());
+        selected.setFirstname(us.getFirstname());
+        selected.setLastname(us.getLastname());
+
+        return "edit";
+    }
+
+    public String save(){
+
+        if(selected != null){
+
+            //ArrayList<User> list = users.getDb();
+            int index = list.indexOf(selected);
+            if (index >= 0)
+            {
+                list.set(index,selected);
+                //users.setDb(list);
+                selected = new User();
+                return "list";
+            }
+
+            return "edit";
+
+        }
+        else {
+            return "edit";
+        }
+    }
+
+    public String cancel(){
+
+        System.out.println("cancel editing: "+selected);
+
+        return "list";
+    }
+
+
+}
---
+++ browser-showcase/src/main/java/com/engagepoint/university/ep2013b/browser/showcase/User.java
@@ -0,0 +1,88 @@
+package com.engagepoint.university.ep2013b.browser.showcase;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: iryna.domachuk
+ * Date: 6/20/13
+ * Time: 6:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class User implements Serializable {
+
+
+    private int id;
+    private String firstname;
+    private  String lastname;
+
+     public User(){
+
+     }
+
+    public User(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        /*
+        if (!firstname.equals(user.firstname)) return false;
+        if (!lastname.equals(user.lastname)) return false;
+        */
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        /*
+        result = 31 * result + firstname.hashCode();
+        result = 31 * result + lastname.hashCode();
+        */
+        return result;
+    }
+}

45b5d7768d104239c85578b3bb5ac471ed1f3bb4
vladimirovcharov <vladimir.ovcharov@engagepoint.com>
2013-07-03 12:34:18 +0300

    tree upgrade

--- src/main/java/core/BrowserComponent.java
+++ src/main/java/core/BrowserComponent.java
@@ -23,22 +23,32 @@ import java.util.List;
 @FacesComponent("browserComponent")
 public class BrowserComponent extends UIComponentBase implements NamingContainer {
      private TreeNode root;
+     private TreeNode node;
 
     @Override
     public String getFamily() {
         return UINamingContainer.COMPONENT_FAMILY;
     }
 
-    //listner onexpand
+    //listener on expand
 
     @Override
     public void encodeBegin(FacesContext arg0) throws IOException {
 
         root = new DefaultTreeNode("Root", null);
         List<BrowserItem> list = Service.getItems();
-        for (BrowserItem browserItem : list) {
-            new DefaultTreeNode(browserItem, root);
-        }
+
+        TreeNode node0 = new DefaultTreeNode(list.get(0), root);
+        TreeNode node1 = new DefaultTreeNode(list.get(1), root);
+        TreeNode node2 = new DefaultTreeNode(list.get(2), root);
+        TreeNode node3 = new DefaultTreeNode(list.get(3), node0);
+        TreeNode node4 = new DefaultTreeNode(list.get(4), node0);
+        TreeNode node5 = new DefaultTreeNode(list.get(5), node1);
+        TreeNode node6 = new DefaultTreeNode(list.get(6), node5);
+        TreeNode node7 = new DefaultTreeNode(list.get(7), node3);
+        TreeNode node8 = new DefaultTreeNode(list.get(8), node7);
+        TreeNode node9 = new DefaultTreeNode(list.get(9), node3);
+
         super.encodeBegin(arg0);
     }
 

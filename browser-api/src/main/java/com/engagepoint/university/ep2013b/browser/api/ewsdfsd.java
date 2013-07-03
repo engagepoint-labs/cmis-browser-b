67f66d7a95924b5d4099c38a60cfceb559b6fb9a
vadymkarko <vadym.karko@engagepoint.com>
2013-07-03 16:28:11 +0300

    SPI improved, divided by modules

---
+++ browser-api/src/main/java/com/engagepoint/university/ep2013b/browser/api/ewsdfsd.java
@@ -0,0 +1,11 @@
+package com.engagepoint.university.ep2013b.browser.api;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vadym.karko
+ * Date: 7/3/13
+ * Time: 4:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ewsdfsd {
+}

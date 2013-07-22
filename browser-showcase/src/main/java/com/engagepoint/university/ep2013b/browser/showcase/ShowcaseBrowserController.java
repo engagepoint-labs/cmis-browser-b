944a7633df5b302e43077ed31368d2717600f1a4
eshevchenko <sh_ev_a@ukr.net>
2013-07-22 17:25:10 +0300

    jbehave

---
+++ browser-showcase/src/main/java/com/engagepoint/university/ep2013b/browser/showcase/ShowcaseBrowserController.java
@@ -0,0 +1,19 @@
+package com.engagepoint.university.ep2013b.browser.showcase;
+
+import com.engagepoint.university.ep2013b.browser.component.controller.AbstractBrowserController;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.swing.text.View;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: evgeniy.shevchenko
+ * Date: 7/22/13
+ * Time: 3:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@ManagedBean
+@ViewScoped
+public class ShowcaseBrowserController extends AbstractBrowserController{
+}

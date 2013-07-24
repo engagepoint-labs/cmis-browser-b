e19a7b7401f26822618e19ce0465896baf6b91c3
vadymkarko <vadym.karko@engagepoint.com>
2013-07-24 14:58:46 +0300

    Added default url

--- src/main/java/com/engagepoint/labs/b/preferences/PreferencesHelper.java
+++ src/main/java/com/engagepoint/labs/b/preferences/PreferencesHelper.java
@@ -39,7 +39,7 @@ public class PreferencesHelper
     public void setCmisUrl(String url)
     {
         // check if url is correct
-        String pattern = "^(https?)://((\\w|\\.\\w)*):(\\d{4})((?:/\\w+(?:_\\w+)*(?:-\\w+)*)+)/$";
+        String pattern = "^(https?)://((\\w|\\.\\w)*)(:(\\d{2,5}))?((?:/\\w+(?:_\\w+)*(?:-\\w+)*)+)/$";
         if (!url.matches(pattern)) throw new IllegalArgumentException();
 
         settings.put(CMIS_URL, url);
